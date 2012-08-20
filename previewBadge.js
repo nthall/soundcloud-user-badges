06f34d568de1541692d2a5083f8bb4fb3b54f256
Noah Hall <noah.t.hall@gmail.com>
2012-08-19 19:22:09 -0700

    moved doc.ready func to bottom of file

--- previewBadge.js
+++ previewBadge.js
@@ -3,36 +3,6 @@ client_id = "44361aaca81d0d37fa071536376d14d4";
 root_url = "https://soundcloud.com";
 selectors = ".userImageBadge, .soundTitle__username, .userBadge__title, .commentItem > .sc-media-image, .commentItem > .commentItem__content > .commentItem__body > .sc-text-light, .commentBadge__body > a, .userNetworkInfo__imageLink";
 
-$(function() {
-  SC.initialize({client_id: client_id});
-  $('body').on('mouseenter', 
-    selectors + ':not(.previewBadge)', 
-    function(event) { 
-      forceVisibility(this);
-      buildPreview(this); 
-      });
-  if (!dev) {
-  $('body').on('mouseleave', 
-    selectors + '.previewBadge', 
-      function(event) {
-        if ($(this).hasClass('previewBadge')) {
-          $(this.parent).remove(this);
-        } else {
-          //  setTimeout(function() {
-          $(this).siblings().remove('.previewBadge');
-          //      }, 750);
-        }
-  });
-  }
-  if (dev) {
-    $('body').prepend("<button class='clearPreview' onclick='#'>Clear Badges</button>");
-    $('.clearPreview').on('click', function() {$('body').remove('.previewBadge')});
-  }
-        //todo: stop execution on buildPreview if mouseleave before setTimeout.
-        //todo: figure out something that both prevents recursion
-        ///////   AND doesn't remove the badge until mouse has left 
-        ///////   BOTH the triggering element AND the badge.
-});
 
 function forceVisibility(el) {
   //may be best to determine which elements to change on a per-class basis (like findLink below)
@@ -74,3 +44,34 @@ function buildPreview(el) {
 }
 
 function cl(msg) {if (dev) {console.log(msg);}}
+
+$(function() {
+  SC.initialize({client_id: client_id});
+  $('body').on('mouseenter', 
+    selectors + ':not(.previewBadge)', 
+    function(event) { 
+      forceVisibility(this);
+      buildPreview(this); 
+      });
+  if (!dev) {
+  $('body').on('mouseleave', 
+    selectors + '.previewBadge', 
+      function(event) {
+        if ($(this).hasClass('previewBadge')) {
+          $(this.parent).remove(this);
+        } else {
+          //  setTimeout(function() {
+          $(this).siblings().remove('.previewBadge');
+          //      }, 750);
+        }
+  });
+  }
+  if (dev) {
+    $('body').prepend("<button class='clearPreview' onclick='#'>Clear Badges</button>");
+    $('.clearPreview').on('click', function() {$('.previewBadge').remove()});
+  }
+        //todo: stop execution on buildPreview if mouseleave before setTimeout.
+        //todo: figure out something that both prevents recursion
+        ///////   AND doesn't remove the badge until mouse has left 
+        ///////   BOTH the triggering element AND the badge.
+});
